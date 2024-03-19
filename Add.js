765d1d6a663e597a9134506bed65b638791eb8a6
Naveen Prakash Ulaganathan <Naveen.Ulaganathan@ATMECS.CORP>
2024-03-19 15:11:58 +0530

    initial commit

---
+++ Add.js
@@ -0,0 +1,18 @@
+function twoFer(person = "You") {
+    return 'One for ' + person + ', one for me';
+}
+console.log(twoFer());
+console.log(twoFer("Naveen"));
+const isLeapYear = (year) => {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    else if (year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+console.log(isLeapYear(2012));
+console.log(isLeapYear(2013));
